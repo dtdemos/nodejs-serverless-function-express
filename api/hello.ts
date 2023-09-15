84473b7f54784e10f6ccba26b2010471d11b3ae9
Rob Jahn <rob.jahn@dynatrace.com>
2023-09-15 16:48:46 -0400

    Otel updates

--- api/hello.ts
+++ api/hello.ts
@@ -1,9 +1,69 @@
+import '../instrumentation'
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+const opentelemetry = require('@opentelemetry/api');
+
+// Name your tracer whatever you want
+const tracer =  opentelemetry.trace.getTracer('mytracer');
+
+function doThis(parentSpan, i) {
+
+  // Get active context
+  // Will be used when child span is created
+  const ctx = opentelemetry.trace.setSpan(opentelemetry.context.active(), parentSpan);
+
+  const childSpan = tracer.startSpan(`doThis${i}`, undefined, ctx);
+  console.log(`Doing this... ${i}`);
+
+  childSpan.addEvent(`childSpan${i} event`, {
+    // Dynatrace will see 'exception.message' a valid span event
+    'exception.message': `some childSpan${i} event message`,
+  }); 
+
+  childSpan.end();
+
+}
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+
+  const { name = 'Dynatrace User' } = req.query  
+
+  // Set the response but do not return yet
+  res.json({
+    message: `Hello ${name}!`,
+  });
+
+  // Start the outer "parent" span
+  const parentSpan = tracer.startSpan('parent-span');
+
+  // set some span attributes
+  parentSpan.setAttribute("AttributeA", "ValueA");
+  parentSpan.setAttribute("AttributeB", "ValueB");
+  parentSpan.setAttribute("AttributeC", "ValueC");
+
+  for (let counter = 1; counter <= 2; counter += 1) {
+    // Call a function and pass the parentSpan
+    // Because child spans will be made for each
+    // Of these "inner" loops
+    doThis(parentSpan, counter);
+  }
+
+  // End the parent span
+  parentSpan.end()
+}
+
+/*
+
+// TRIED THIS ORGINALLY AND IT DID NOT WORK FOR A REMOTE COLLECTOR
+
 import { tracer } from '../instrumentation';
 import { SpanStatusCode } from '@opentelemetry/api';
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  
   // start Otel outer span
   return tracer.startActiveSpan('outer-span', (parentspan) => {
   
@@ -45,3 +105,5 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   }
   )  
 }
+
+*/
--- instrumentation.ts
+++ instrumentation.ts
@@ -1,3 +1,37 @@
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
+const exporterUrl = process.env.EXPORTER_URL;
+console.log('exporterUrl = ' + exporterUrl)
+
+const provider = new BasicTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'rob-demo-service',
+  }),
+});
+
+
+const exporter = new OTLPTraceExporter({
+  url: exporterUrl,
+});
+
+// Send spans to the exporter endpoint
+// AND the console
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+provider.register();
+
+
+/*
+
+// TRIED THIS ORGINALLY AND IT DID NOT WORK FOR A REMOTE COLLECTOR
+
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { Resource } from '@opentelemetry/resources';
@@ -6,22 +40,24 @@ import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node';
 import opentelemetry from '@opentelemetry/api';
 
 export const tracer = opentelemetry.trace.getTracer(
-  'vercel nodejs-serverless-function-express instrumentation-scope-name',
-  'vercel nodejs-serverless-function-express instrumentation-scope-version',
+  'vercel-nodejs-serverless-function-express-instrumentation-scope-name',
+  'vercel-nodejs-serverless-function-express-instrumentation-scope-version',
 );
 
 // Set the EXPORTER_URL within .env.local
 const exporterUrl = process.env.EXPORTER_URL;
+console.log('exporterUrl = ' + exporterUrl)
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter({
     url: exporterUrl,
-    // optional - collection of custom headers to be sent with each request, empty by default
-    headers: {},
+    headers: {}, 
   }),
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'vercel nodejs-serverless-function-express',
+    [SemanticResourceAttributes.SERVICE_NAME]: 'vercel-nodejs-serverless-function-express',
   }),
   spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
 });
 sdk.start();
 console.log(new Date().toUTCString() + " instrumentation sdk.start()")
+
+*/
